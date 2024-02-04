e4c0c488ab02a55a719cf36c379c1aa18f30e5cf
MohamedYehia20m <medoyehia2001@gmail.com>
2024-02-04 20:50:02 +0200

    add user module

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""User module"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User class"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of User instance"""
+        super().__init__(*args, **kwargs)
